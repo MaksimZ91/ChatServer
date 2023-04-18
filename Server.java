d1d3fc16e0bf846344a2c2c1a433722bd44e8df3
MaksimZ91 <ak4202@mail.ru>
2023-04-18 21:07:47 +0800

    compl

--- Server.java
+++ Server.java
@@ -5,17 +5,17 @@ import java.util.LinkedList;
 
 class  Server{
     private static final int PORT = 58421;
-    public static LinkedList<Servers> ServerList = new LinkedList<Servers>();
+    public static LinkedList<Servers> ServerList = new LinkedList<Servers>(); //Список созданых сокетов
 
 
     public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(PORT);
+        ServerSocket server = new ServerSocket(PORT); //Содание Сервер скоет и прослушиваем порт 58421
         System.out.println("Server start");
         try {
             while (true) {
-                Socket socket = server.accept();
+                Socket socket = server.accept(); //server.accept() блокирующая функции, ждет рукопожатия на выходе создает сокет соединение.
                 try {
-                    ServerList.add(new Servers(socket));
+                    ServerList.add(new Servers(socket)); //добавляем новое соединение в лист и создаем для него свой поток
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
@@ -33,9 +33,9 @@ class Servers extends Thread{
 
     public  Servers(Socket socket) throws IOException {
         this.socket = socket;
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()),1024);
-        start();
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream())); //стрим для чтения данных
+        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()),1024); //стрим для отправки данных
+        start(); // запуск потока при иницализаци конструктора.
     }
 
     @Override
@@ -43,9 +43,9 @@ class Servers extends Thread{
         String message;
         while (true){
             try {
-                message = in.readLine();
+                message = in.readLine(); //читаем все из буфера если есть что читать
                 System.out.println(message);
-                for(Servers sr : Server.ServerList){
+                for(Servers sr : Server.ServerList){ //рассылаем сообщение на все сокеты изспика
                     sr.send(message);
                 }
             } catch (IOException e) {
